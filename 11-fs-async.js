10d4f96210d425d425ea28606da95348cbb30b20
Ugoo <ugonnaezeabara@gmail.com>
2022-08-15 09:17:24 +0100

    added comments to fs-async.js file

--- 11-fs-async.js
+++ 11-fs-async.js
@@ -1,18 +1,23 @@
 const { readFile, writeFile } = require('fs')
 
 console.log('start')
+// read file inside first.txt file
 readFile('./content/first.txt', 'utf8', (err, result) => {
   if (err) {
     console.log(err)
     return
   }
   const first = result
+
+  // read file inside secoud.txt file
   readFile('./content/second.txt', 'utf8', (err, result) => {
     if (err) {
       console.log(err)
       return
     }
     const second = result
+
+    // write file inside result-async.txt file
     writeFile(
       './content/result-async.txt',
       `Here is the result : ${first}, ${second}`,
--- 9-path-module.js
+++ 9-path-module.js
@@ -1,4 +1,5 @@
 const path = require('path')
+
 console.log(path.sep)
 
 const filePath = path.join('/content/', 'subfolder', 'test.txt')
